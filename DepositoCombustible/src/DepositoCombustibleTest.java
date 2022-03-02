a245c7afb731aeb617a17307a4ac2e201ac3f6cd
rafa <rafarafer@hotmail.com>
2022-03-02 16:59:50 +0100

    consumir

---
+++ DepositoCombustible/src/DepositoCombustibleTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DepositoCombustibleTest {
+
+	@Test
+	void testConsumir() {
+		fail("No implementado aun");
+	}
+
+}
