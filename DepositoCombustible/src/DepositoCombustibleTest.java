58c447fe682abebde9aed455fb54205b94bbd50c
rafa <rafarafer@hotmail.com>
2022-03-02 16:52:07 +0100

    deposito nivel

---
+++ DepositoCombustible/src/DepositoCombustibleTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DepositoCombustibleTest {
+
+	@Test
+	void testGetDepositoNivel() {
+		fail("No implementado aun");
+	}
+
+}
