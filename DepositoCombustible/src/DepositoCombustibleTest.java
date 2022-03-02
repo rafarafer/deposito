6ca9eb5db1be088633dbdda41f662d6d406bb1f9
rafa <rafarafer@hotmail.com>
2022-03-02 16:55:59 +0100

    esta vacio

---
+++ DepositoCombustible/src/DepositoCombustibleTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DepositoCombustibleTest {
+
+	@Test
+	void testEstaVacio() {
+		fail("No implementado aun");
+	}
+
+}
