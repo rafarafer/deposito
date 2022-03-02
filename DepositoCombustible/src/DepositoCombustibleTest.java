1c755643a85658c282b0bd5dddf83c8fc059e422
rafa <rafarafer@hotmail.com>
2022-03-02 16:54:19 +0100

    deposito max

---
+++ DepositoCombustible/src/DepositoCombustibleTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DepositoCombustibleTest {
+
+	@Test
+	void testGetDepositoMax() {
+		fail("No implementado aun");
+	}
+
+}
