a406b21fa6452a354e1161d04995c038f516e4b2
rafa <rafarafer@hotmail.com>
2022-03-11 12:10:37 +0100

    prueba ejecutada

--- DepositoCombustible/src/DepositoCombustibleTest.java
+++ DepositoCombustible/src/DepositoCombustibleTest.java
@@ -6,7 +6,10 @@ class DepositoCombustibleTest {
 
 	@Test
 	void testGetDepositoNivel() {
-		fail("No implementado aun");
+		DepositoCombustible tank = new DepositoCombustible(40.0,20.0);
+		//System.out.print("The tank with capacity " + tank.getDepositoMax() + " liters has been created. ");
+	   // System.out.println(" Its initial fuel level is  " + tank.getDepositoNivel() + " liters."); 
+	    assertEquals(20.0, tank.getDepositoNivel());
 	}
-
+	 
 }
